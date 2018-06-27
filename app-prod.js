2790186913862957c53bed1c2484a6b8dd83d59b
Michael Oria <Michael_Oria@me.com>
2018-06-26 21:13:15 -0400

    Heroku Commit.

--- app-prod.js
+++ app-prod.js
@@ -15,7 +15,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 
 // Loud and Clear
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 // Protect Yoself
 const dotenv = require('dotenv');
